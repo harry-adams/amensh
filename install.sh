9f7cd9853162fb4794221a830ea8b6df1a171f0d
harry-adams <harry.adams@mpb.com>
2022-03-10 16:06:32 +0000

    Add install.sh script

---
+++ install.sh
@@ -0,0 +1,39 @@
+if [ "$SHELL" == "/bin/bash" ];
+then
+cat >> ~/.bashrc <<EOL
+function amensh () {
+  local branch_name=$1
+  git add -A &&
+  git commit --amend --no-edit &&
+  git fetch --all &&
+  git rebase origin/$branch_name &&
+  git push -f
+  }
+EOL
+fi
+if [ "$SHELL" == "/bin/zsh" ];
+then
+cat >> ~/.zshrc <<EOL
+function amensh () {
+    local branch_name=$1
+    git add -A &&
+    git commit --amend --no-edit &&
+    git fetch --all &&
+    git rebase origin/$branch_name &&
+    git push -f
+}
+EOL
+fi
+if [ "$SHELL" == "/bin/fish" ];
+then
+cat >> ~/.config/fish/config.fish <<EOL
+function amensh () {
+    local branch_name=$1
+    git add -A &&
+    git commit --amend --no-edit &&
+    git fetch --all &&
+    git rebase origin/$branch_name &&
+    git push -f
+}
+EOL
+fi
